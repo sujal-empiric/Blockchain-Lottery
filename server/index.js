a287df07982c408c36fb3d9fa772271e4c2b346e
sdakhara <scdakhara123@gmail.com>
2022-11-17 14:43:29 +0530

    Final Commit 2

--- server/index.js
+++ server/index.js
@@ -3,8 +3,9 @@ const path = require("path");
 require('dotenv').config()
 const app = express();
 
+
 app.use(express.static(path.join(__dirname, "../front-end/build")));
-app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "./index.html")));
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "../front-end/build", "index.html")));
 console.log(process.env.HOMIES)
 const PORT = process.env.PORT || 3000;
 
